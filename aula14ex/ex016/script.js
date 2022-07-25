5c3d592ca39ec31aed628c184485f53fef23beef
Mauricio Charaba <108958775+professorcharaba@users.noreply.github.com>
2022-07-25 17:23:08 -0300

    Fim ex016

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,31 @@
-alert('Olá!')
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')    
+    } else {
+        res.innerHTML = 'Contando... <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p < 1) {
+            window.alert('Passo inválido! Mudando para 1')
+            p = 1
+        }
+        if (i < f) {
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` \u{1f449} ${c}`
+            }
+            // Full Emoji List
+        } else {
+            for(let c = i; c >= f; c -= p) {
+                    res.innerHTML += ` \u{1f449} ${c}`
+                }
+            // Full Emoji List            
+        }
+        res.innerHTML += ` \u{1f3c1}`
+    }
+}    
